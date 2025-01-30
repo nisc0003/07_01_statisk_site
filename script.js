fbb3f57a98d0b5ac7a49fd8ed4bf8003360cda59
Nicolai Schou <nisc0003@stud.kea.dk>
2025-01-30 03:05:58 +0100

    add ændringer

--- script.js
+++ script.js
@@ -35,3 +35,19 @@ document.querySelector(".cta-button").addEventListener("click", function () {
     this.textContent = "Hide Details"; // Change button text to "Hide Details"
   }
 });
+
+// burger menu
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", burgerClick);
+function burgerClick() {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+menu.addEventListener("click", menuClick);
+function menuClick() {
+  burger.classList.remove("active");
+  nav.classList.remove("active");
+}
